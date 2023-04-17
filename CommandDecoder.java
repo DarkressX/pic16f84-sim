5acbf36c9b2a2a0e48cc1109057e347c2430c04d
darkress <github@darkress.xyz>
2023-04-17 16:18:14 +0200

    Added CommandDecoder for all Byte-oriented file register operations

--- CommandDecoder.java
+++ CommandDecoder.java
@@ -25,6 +25,39 @@ public class CommandDecoder
         } else if ((binaryInput & 0x3F00) == 0xB00)
         {
             //DECFSZ();
+        } else if ((binaryInput & 0x3F00) == 0xA00)
+        {
+            //INCF();
+        } else if ((binaryInput & 0x3F00) == 0xF00)
+        {
+            //INCFSZ();
+        } else if ((binaryInput & 0x3F00) == 0x400)
+        {
+            //IORWF();
+        } else if ((binaryInput & 0x3F00) == 0x800)
+        {
+            //MOVF();
+        } else if ((binaryInput & 0x3F80) == 0x80)
+        {
+            //MOVWF();
+        } else if ((binaryInput & 0x7FF) == 0x0)
+        {
+            //NOP();
+        } else if ((binaryInput & 0x3F00) == 0xD00)
+        {
+            //RLF();
+        } else if ((binaryInput & 0x3F00) == 0xC00)
+        {
+            //RRF();
+        } else if ((binaryInput & 0x3F00) == 0x200)
+        {
+            //SUBWF();
+        } else if ((binaryInput & 0x3F00) == 0xE00)
+        {
+            //SWAPF();
+        } else if ((binaryInput & 0x3F00) == 0x600)
+        {
+            //XORWF();
         }
     }
 }
