c1a51c4d94e0b815f60b1b8522ee16f70e3f2965
Darkress <github@darkress.xyz>
2023-05-09 22:44:59 +0200

    Implemented Program Parser

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        int input = Integer.decode("0x0070");
-        CommandDecoder commandDecoder = new CommandDecoder(input);
+        System.out.println(Parser.parser("TPicSim2.LST"));
     }
 }
