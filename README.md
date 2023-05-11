0ab0c6d4714db64a52821c99441ce6b16292a532
Darkress <github@darkress.xyz>
2023-05-11 20:46:34 +0200

    Added some abbreviations and registers of bank0

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # pic16f84-sim
+
+FSR = File select register
+
+SFR = Special function register
+
+W = Working register
+
+Status = Status register
+
+GPR = General purpose register; Directly or indirectly accessible through FSR
+
+C = Carry > Status register
+DC = Digit Carry > Status register
+Z = ZeroFlag > Status register
+
+INTCON = Interrupt Condition(?) register
+
+SRAM in Bank1 is mappend to Bank0
--- Registers.java
+++ Registers.java
@@ -1,8 +1,113 @@
 public class Registers
 {
-    public static short zeroBit;
-    public static void setZeroBit(int input)
-    {
+    private static short TMR0;
+    private static short PCL;
+    private static short STATUS;
+    private static short FSR;
+    private static short PORTA;
+    private static short PORTB;
+    private static short EEDATA;
+    private static short EEADR;
+    private static short PCLATH;
+    private static short INTCON;
 
+    public static short getTMR0()
+    {
+        return TMR0;
+    }
+
+    public static void setTMR0(short TMR0)
+    {
+        Registers.TMR0 = TMR0;
+    }
+
+    public static short getPCL()
+    {
+        return PCL;
+    }
+
+    public static void setPCL(short PCL)
+    {
+        Registers.PCL = PCL;
+    }
+
+    public static short getSTATUS()
+    {
+        return STATUS;
+    }
+
+    public static void setSTATUS(short STATUS)
+    {
+        Registers.STATUS = STATUS;
+    }
+
+    public static short getFSR()
+    {
+        return FSR;
+    }
+
+    public static void setFSR(short FSR)
+    {
+        Registers.FSR = FSR;
+    }
+
+    public static short getPORTA()
+    {
+        return PORTA;
+    }
+
+    public static void setPORTA(short PORTA)
+    {
+        Registers.PORTA = PORTA;
+    }
+
+    public static short getPORTB()
+    {
+        return PORTB;
+    }
+
+    public static void setPORTB(short PORTB)
+    {
+        Registers.PORTB = PORTB;
+    }
+
+    public static short getEEDATA()
+    {
+        return EEDATA;
+    }
+
+    public static void setEEDATA(short EEDATA)
+    {
+        Registers.EEDATA = EEDATA;
+    }
+
+    public static short getEEADR()
+    {
+        return EEADR;
+    }
+
+    public static void setEEADR(short EEADR)
+    {
+        Registers.EEADR = EEADR;
+    }
+
+    public static short getPCLATH()
+    {
+        return PCLATH;
+    }
+
+    public static void setPCLATH(short PCLATH)
+    {
+        Registers.PCLATH = PCLATH;
+    }
+
+    public static short getINTCON()
+    {
+        return INTCON;
+    }
+
+    public static void setINTCON(short INTCON)
+    {
+        Registers.INTCON = INTCON;
     }
 }
