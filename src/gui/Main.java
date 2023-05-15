89ff18256fd4bb23083c0c5e0e5938efa17b9273
L2S8 <l.schablack@netze-bw.de>
2023-05-15 16:01:04 +0200

    file opener attempt1

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,15 +1,71 @@
 package src.gui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
+
+    static JFileChooser fc = new JFileChooser();
+
     public static void main(String[] args)
     {
+
+
         JFrame mainFrame = new JFrame("pic16f84-sim");
 
-        mainFrame.setSize(200,200);
+        mainFrame.setSize(900,800);
         mainFrame.add(new JLabel("Beispiel JLabel"));
         mainFrame.setVisible(true);
+
+        JMenuBar mb = new JMenuBar();
+        final JMenu fileMenu = new JMenu("FILE");
+        mb.add(fileMenu);
+        JMenuItem open = new JMenuItem("Open");
+        fileMenu.add(open);
+
+        JPanel ltePanel = new JPanel();
+        mainFrame.add(ltePanel);
+
+        JScrollPane lte = new JScrollPane();
+
+
+        open.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+
+                int chooser = fc.showOpenDialog(null);
+                if(chooser == JFileChooser.APPROVE_OPTION)
+                    displayFile(fc.getSelectedFile(), ltePanel);
+
+            }
+        });
+
+
+
+
+        mainFrame.getContentPane().add(BorderLayout.NORTH, mb);
+
+    }
+
+    public static void displayFile(File file, JPanel panel){
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fc.getSelectedFile().getPath()));
+            String line = reader.readLine();
+
+            while (line != null) {
+                panel.add(new JLabel(line + "\n"));
+                line = reader.readLine();
+            }
+
+
+            reader.close();
+
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
     }
 }
 
