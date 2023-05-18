13c6066dee56025d51b8e9dcabad3dbfac3ef2fd
Darkress <github@darkress.xyz>
2023-05-18 13:26:25 +0200

    Working Memory with direct and indirect addressing

--- Registers.java
+++ Registers.java
@@ -3,22 +3,58 @@ import java.util.Arrays;
 public class Registers
 {
     private static final int MEMORY_SIZE = 0xFF;  //Addressable Memory
-    //private static int bankSelect = 0;
     private static final int[] memory = new int[MEMORY_SIZE];
     private static final int[] bank0UniqueSpecialRegister = new int[] {0x01,0x05,0x06,0x08,0x09}; //and many more
     private static final int[] bank1UniqueSpecialRegister = new int[] {0x81,0x85,0x86,0x88,0x89}; //and many more
 
     public static int getRegister(int address)
     {
-        return 5;
+        if(address == 0x0 || address == 0x80) //0x80 probably not needed because of 7-Bit address
+        {
+            int indirectAddress = getFSR();
+            return getDataFromIndirectAddress(indirectAddress);
+        }
+        if(getRegisterBank() == 0)
+        {
+            return memory[address];
+        } else if(getRegisterBank() == 1)
+        {
+            if((Arrays.stream(bank1UniqueSpecialRegister).anyMatch(x -> x == address)))
+            {
+                return memory[address + 128]; //Write to correct memory address
+            }
+            return memory[address]; // else: Registers which are mapped
+        }
+        System.out.println("This should never happen");
+        return 0xFF;
     }
 
     public static void setRegister(int address, int data)
     {
-
+        if(address == 0x0 || address == 0x80)
+        {
+            int indirectAddress = getFSR();
+            setDataFromIndirectAddress(indirectAddress, data);
+            return;
+        }
+        if(getRegisterBank() == 0)
+        {
+            memory[address] = data;
+            return;
+        } else if(getRegisterBank() == 1)
+        {
+            if((Arrays.stream(bank1UniqueSpecialRegister).anyMatch(x -> x == address)))
+            {
+                memory[address + 128] = data; //Write to correct memory address
+                return;
+            }
+            memory[address] = data; // else: Registers which are mapped
+            return;
+        }
+        System.out.println("This should never happen");
     }
 
-    public static int getDataFromIndirectAddress(int address)
+    private static int getDataFromIndirectAddress(int address)
     {
         if((Arrays.stream(bank0UniqueSpecialRegister).anyMatch(x -> x == address)) || (Arrays.stream(bank1UniqueSpecialRegister).anyMatch(x -> x == address)))
         {
@@ -27,7 +63,7 @@ public class Registers
         return memory[address % 128]; // else: Registers which are mapped
     }
 
-    public static void setDataFromIndirectAddress(int address, int data)
+    private static void setDataFromIndirectAddress(int address, int data)
     {
         if((Arrays.stream(bank0UniqueSpecialRegister).anyMatch(x -> x == address)) || (Arrays.stream(bank1UniqueSpecialRegister).anyMatch(x -> x == address)))
         {
@@ -36,13 +72,17 @@ public class Registers
         memory[address % 128] = data; // else: Registers which are mapped
     }
 
-    private static int checkRegisterBank()
+    private static int getRegisterBank()
     {
-        //syncStatusRegister();
         if((memory[0x03] & 0x20) == 0x0)  //Check RP0 Bit of Bank0 Status Register
         {
             return 0;
         }
         return 1;
+    }
+
+    private static int getFSR()
+    {
+        return memory[0x4];
     }
 }
