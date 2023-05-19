489470a338ece8b64fdee0734d3fac391254d55f
Darkress <github@darkress.xyz>
2023-05-19 22:18:12 +0200

    Added working register

--- Registers.java
+++ Registers.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Registers
 {
     private static final int MEMORY_SIZE = 0xFF;  //Addressable Memory
+    public static final int workingRegister = 0;
     private static final int[] memory = new int[MEMORY_SIZE];
     private static final int[] bank0UniqueSpecialRegister = new int[] {0x01,0x05,0x06,0x08,0x09}; //and many more
     private static final int[] bank1UniqueSpecialRegister = new int[] {0x81,0x85,0x86,0x88,0x89}; //and many more
@@ -73,5 +74,62 @@ public class Registers
     private static int getFSR()
     {
         return memory[0x4];
+    }
+
+    public static boolean getZeroBit()
+    {
+        if((memory[0x03] & 0x04) == 0x04)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static void setZeroBit()
+    {
+        memory[0x03] |= 0x04;
+    }
+
+    public static void clearZeroBit()
+    {
+        memory[0x03] &= 0xFB;
+    }
+
+    public static boolean getDigitCarryBit()
+    {
+        if((memory[0x03] & 0x02) == 0x02)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static void setDigitCarryBit()
+    {
+        memory[0x03] |= 0x02;
+    }
+
+    public static void clearDigitCarryBit()
+    {
+        memory[0x03] &= 0xFD;
+    }
+
+    public static boolean getCarryBit()
+    {
+        if((memory[0x03] & 0x01) == 0x01)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static void setCarryBit()
+    {
+        memory[0x03] |= 0x01;
+    }
+
+    public static void clearCarryBit()
+    {
+        memory[0x03] &= 0xFE;
     }
 }
