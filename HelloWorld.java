2d16c02c04316faab8ca1f1e5a5713c887171298
Darkress <github@darkress.xyz>
2023-05-22 23:54:34 +0200

    Added example execution of Addlw command

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,13 @@
-import Commands.Addlw;
-import Commands.Command;
-import Registers.Memory;
+import commands.Command;
+import decoder.CommandDecoder;
 
 import java.util.ArrayList;
 
 class HelloWorld {
     public static void main(String[] args) {
-        //System.out.println(Parser.parser("TPicSim2.LST"));
+        //System.out.println(Parser.Parser.parser("TPicSim2.LST"));
         ArrayList<Command> program = new ArrayList<>();
-        int input1 = 0x3EFE;
+        int input1 = 0x3EFF;
         program.add(CommandDecoder.decode(input1));
         program.get(0).execute();
     }
