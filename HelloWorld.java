a08a336864fb2aa2bbc5a4e37ca360765774965e
Darkress <github@darkress.xyz>
2023-05-22 23:51:37 +0200

    Revert changes on HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,7 @@
-import Commands.Addlw;
-import Commands.Command;
-import Registers.Memory;
 
-import java.util.ArrayList;
 
 class HelloWorld {
     public static void main(String[] args) {
-        //System.out.println(Parser.parser("TPicSim2.LST"));
-        ArrayList<Command> program = new ArrayList<>();
-        int input1 = 0x3EFE;
-        program.add(CommandDecoder.decode(input1));
-        program.get(0).execute();
+        System.out.println(Parser.parser("TPicSim2.LST"));
     }
 }
