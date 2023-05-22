e53bf5d89779a5d232cdfd7c8584222b54fbf6a5
Darkress <github@darkress.xyz>
2023-05-22 23:52:31 +0200

    Revert "Revert changes on HelloWorld"
    This reverts commit a08a336864fb2aa2bbc5a4e37ca360765774965e.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,15 @@
+import Commands.Addlw;
+import Commands.Command;
+import Registers.Memory;
 
+import java.util.ArrayList;
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println(Parser.parser("TPicSim2.LST"));
+        //System.out.println(Parser.parser("TPicSim2.LST"));
+        ArrayList<Command> program = new ArrayList<>();
+        int input1 = 0x3EFE;
+        program.add(CommandDecoder.decode(input1));
+        program.get(0).execute();
     }
 }
