f6111a94d02aff6359820302a8d54692c2faa3d8
Darkress <git@darkress.xyz>
2024-03-18 21:09:56 +0100

    Restore Nop instruction

---
+++ src/main/java/de/darkress/pic16f84sim/commands/Nop.java
@@ -0,0 +1,16 @@
+package de.darkress.pic16f84sim.commands;
+
+import de.darkress.pic16f84sim.microcontroller.Cycles;
+import de.darkress.pic16f84sim.microcontroller.ProgramCounter;
+
+public class Nop extends LiteralCommandUtils implements Command
+{
+
+    @Override
+    public void execute()
+    {
+        // Do nothing, just increment the PC
+        ProgramCounter.incPC();
+        Cycles.incCycles();
+    }
+}
